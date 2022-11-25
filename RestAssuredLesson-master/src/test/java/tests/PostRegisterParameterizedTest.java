6d87fc0332a8e583da4bacdc8ea18b5c1af5d8e3
Vlad Ivanov <vlad.vonavi@gmial.com>
2022-11-25 09:41:01 +0300

    Правки по прошлым работам.

--- RestAssuredLesson-master/src/test/java/tests/PostRegisterParameterizedTest.java
+++ RestAssuredLesson-master/src/test/java/tests/PostRegisterParameterizedTest.java
@@ -20,55 +20,56 @@ public class PostRegisterParameterizedTest {
 
     @ParameterizedTest
     @DisplayName("Тест Value Source")
-    @ValueSource(strings={"sobaka@ya.ru", "123456"})
-    void postValueSourceTest(String email){
+    @ValueSource(strings = {"sobaka@ya.ru", "123456"})
+    void postValueSourceTest(String email) {
         registerReqDto.setEmail(email);
         Response postResponse = postRegister(registerReqDto);
         //checkFieldValue(postResponse,"email", registerReqDto.getEmail());
         checkFieldNotNull(postResponse, "error");
-        checkFieldValue(postResponse,"error", "Note: Only defined users succeed registration");
+        checkFieldValue(postResponse, "error", "Note: Only defined users succeed registration");
     }
 
     //@ParameterizedTest(name="Проверка токена для email \"{0}\" и паролем \"{1}\"")
     @ParameterizedTest
     @DisplayName("Тест CsvSource")
-    @CsvSource(value={"sobaka@ya.ru ; 123456", "kot@gmail.com ; 654321"}, delimiter = ';')
-    void postCsvSourceTest(String email, String password){
+    @CsvSource(value = {"sobaka@ya.ru ; 123456", "kot@gmail.com ; 654321"}, delimiter = ';')
+    void postCsvSourceTest(String email, String password) {
         registerReqDto.setEmail(email);
         registerReqDto.setPassword(password);
         Response postResponse = postRegister(registerReqDto);
         //checkFieldValue(postResponse,"email", registerReqDto.getEmail());
         checkFieldNotNull(postResponse, "error");
-        checkFieldValue(postResponse,"error", "Note: Only defined users succeed registration");
+        checkFieldValue(postResponse, "error", "Note: Only defined users succeed registration");
     }
 
     //@ParameterizedTest(name="Проверка токена для email \"{0}\" и паролем \"{1}\"")
     @ParameterizedTest
     @DisplayName("Тест CsvSource")
     @EnumSource(RegisterRequestParams.class)
-    void postEnumTest(RegisterRequestParams registerRequestParams){
+    void postEnumTest(RegisterRequestParams registerRequestParams) {
         registerReqDto.setEmail(String.valueOf(registerRequestParams.Email));
         registerReqDto.setPassword(String.valueOf(registerRequestParams.Password));
         Response postResponse = postRegister(registerReqDto);
         //checkFieldValue(postResponse,"email", registerReqDto.getEmail());
         checkFieldNotNull(postResponse, "error");
-        checkFieldValue(postResponse,"error", "Note: Only defined users succeed registration");
+        checkFieldValue(postResponse, "error", "Note: Only defined users succeed registration");
     }
 
-    public static Stream<Arguments> requestsObjects(){
+    public static Stream<Arguments> requestsObjects() {
         return Stream.of(
-               Arguments.of(registerDto, registerDto.getEmail())
-       );
+                Arguments.of(registerReqDto, registerReqDto.getEmail())
+        );
     }
 
     //@ParameterizedTest(name="Провека токена для email {1}")
     @ParameterizedTest
     @DisplayName("Тест MethodSource")
     @MethodSource("requestsObjects")
-    void postMethodSourceTest(RegisterReqDto registerReqDto, String email){
+    void postMethodSourceTest(RegisterReqDto registerReqDto, String email) {
         registerReqDto.setEmail(email);
         postRegister(registerReqDto);
-   // }
+        // }
+    }
 }
 
 
